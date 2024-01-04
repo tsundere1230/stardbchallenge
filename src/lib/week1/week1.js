5c7877ce62b5b887ddd5777f73eaf9da55ed41c7
tsundere1230 <hui6nam21@gmail.com>
2024-01-05 00:09:52 +0800

    week2update

--- src/lib/week1/week1.js
+++ src/lib/week1/week1.js
@@ -8,14 +8,19 @@ let answers = ["9702", "24000", "88050", "cryptic"];
 let questions = [Question1, Question2, Question3, Question4];
 
 /** @type {Array<{ [key: string]: string }>} */
-let hints = [{}, {}, {}, {}]
+let hints = [{ "9701.999975": "Lols the beta tester expected this so you're getting this cheeky message!" },
+            { "5": "Kafka's VA is 5" },
+            {
+                "200": "IPC is not 200",
+                "300": "IPC is 300"
+            }, {}]
 
 let congrats = `👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit
                 right👌👌th 👌 ere👌👌👌 right✔there ✔✔if i do ƽaү so my selｆ 💯 i
                 say so 💯 thats what im talking about right there right there(chorus:
                     ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠＯOOＯOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯
                 👌 👀 👀 👀 👌👌Good shit
-                    < br /> <br />Congrats nerd! < br /> Now screenshot this page to prove that
+                    <br/> <br/> Congrats nerd! <br/> Now screenshot this page to prove that
                 you have completed the challenge. :) `
 
 let week1 = { questions, answers, congrats, hints };
