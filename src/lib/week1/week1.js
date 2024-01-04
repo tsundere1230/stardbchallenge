302d9d6b23ce0e15d3c7eaf727a84c7425fdb701
tsundere1230 <hui6nam21@gmail.com>
2024-01-05 00:46:25 +0800

    hints

--- src/lib/week1/week1.js
+++ src/lib/week1/week1.js
@@ -8,10 +8,10 @@ let answers = ["9702", "24000", "88050", "cryptic"];
 let questions = [Question1, Question2, Question3, Question4];
 
 /** @type {Array<{ [key: string]: string }>} */
-let hints = [{ "9701.999975": "Lols the beta tester expected this so you're getting this cheeky message!" },
-            { "5": "Kafka's VA is 5" },
+let hints = [{ "1": "Yukong Children is 1.", "3": "Playable Stellaron Hunters is 3.", "9701.999975": "Lols the beta tester expected this so you're getting this cheeky message!" },
+            { "5": "Kafka's VA is 5", "60": "60 is a correct value", "150": "150 is a correct value.", "400": "400 is a correct value." },
             {
-                "200": "IPC is not 200",
+                "200": "IPC is NOT 200",
                 "300": "IPC is 300"
             }, {}]
 
