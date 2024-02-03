b48597827886ae22e82281e3b006ed0981c1062b
Julius Kreutz <julius@kreutz.dev>
2024-02-04 00:00:39 +1100

    Preparing week 3

--- src/lib/weeks.js
+++ src/lib/weeks.js
@@ -1,8 +1,8 @@
 import questions1 from "$lib/week1/questions";
 import questions2 from "$lib/week2/questions";
 import questions3 from "$lib/week3/questions";
-import questions4 from "$lib/week4/questions";
+// import questions4 from "$lib/week4/questions";
 
-const weeks = [questions1, questions2, questions3, questions4];
+const weeks = [questions1, questions2, questions3];
 
 export default weeks;
