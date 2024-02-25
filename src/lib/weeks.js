7acca60246fb8d94205ddb0b2f34eff4ee3ad508
Julius Kreutz <julius@kreutz.dev>
2024-02-26 01:22:58 +1100

    changed image

--- src/lib/weeks.js
+++ src/lib/weeks.js
@@ -2,7 +2,8 @@ import questions1 from "$lib/week1/questions";
 import questions2 from "$lib/week2/questions";
 import questions3 from "$lib/week3/questions";
 import questions4 from "$lib/week4/questions";
+import questions5 from "$lib/week5/questions";
 
-const weeks = [questions1, questions2, questions3, questions4];
+const weeks = [questions1, questions2, questions3, questions4, questions5];
 
 export default weeks;
